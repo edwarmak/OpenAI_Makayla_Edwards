2735d70e61222b687b72e563b09ca10e2e7832b2
Makayla Edwards <mackedwards@Makaylas-MacBook-Air.local>
2022-05-19 16:46:54 -0600

    basic form layout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    alert('Sumbitted')
+  }
+
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h2>Pls work :(</h2>
+      <h1>OpenAI App</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
+            <input type="text"></input>
+            </label>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+
+        <div id="responses">
+
+        </div>
+
     </div>
   );
 }
