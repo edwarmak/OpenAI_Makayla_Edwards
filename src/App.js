68d7b5a020c1d1d07b83c48cbb4cf0573d383eae
Makayla Edwards <mackedwards@Makaylas-MacBook-Air.local>
2022-05-20 12:01:46 -0600

    push

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
-function App() {
+console.log(process.env.OPENAI_API_KEY)
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    alert('Sumbitted')
+const App = () => {
+  const [prompt, setPrompt] = useState('Prompt')
+  const [answer, setAnswer] = useState('')
+
+  let changePrompt = (event) => {
+    setPrompt(event.target.value)
   }
+
+  let getAnswer = () => {
+    axios.get('https://beta.openai.com/examples/default-qa').then((response) => {
+      setAnswer(response.data.answer)
+    })
+  }
+
+  useEffect(() => {
+    getAnswer()
+  }, [])
 
   return (
     <div className="App">
       <h1>OpenAI App</h1>
-
-        <form onSubmit={handleSubmit}>
+        <form>
           <div>
             <label>
-            <input type="text"></input>
+            <input type="text" onChange={changePrompt}></input>
             </label>
           </div>
           <button type="submit">Submit</button>
         </form>
 
         <div id="responses">
-
+          <h1>Responses</h1>
+          <div>
+            <h4>Prompt:</h4>
+              <p>{prompt}</p>
+            <h4>Response:</h4>
+              <p>{answer}</p>
+          </div>
         </div>
 
     </div>
